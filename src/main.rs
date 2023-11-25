bc6fa7931d260240a8a7105f294efe7988264abf
mirged <ntaip004@gmail.com>
2023-11-25 19:57:42 +0200

    Init Commit

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+mod run;
+
+
+fn parse_operand(operand: &str) -> u8 {
+    operand.parse().unwrap_or_else(|_| {
+        panic!("Failed to parse operand: {}", operand);
+    })
+}
+
+fn lexer(mut source: String) -> Vec<u8> {
+    let mut program = Vec::new();
+    source.retain(|c| c != '\n');
+    let parts = source.split(";");
+    for part in parts {
+        let mut tokens = part.split_whitespace();
+        if let Some(opcode_str) = tokens.next() {
+            let opcode: u8 = match opcode_str {
+                "MovRegReg" => 0,
+                "AddRegReg" => 1,
+                "SubRegReg" => 2,
+                "IncReg" => 3,
+                "DecReg" => 4,
+                "JmpAddr" => 5,
+                "HLT" => 6,
+                _ => panic!("Unknown opcode: {}", opcode_str),
+            };
+
+            let mut operands = Vec::new();
+            while let Some(operand_str) = tokens.next() {
+                operands.push(parse_operand(operand_str));
+            }
+
+            // Add default operands (0) for instructions with missing operands
+            while operands.len() < 2 {
+                operands.push(0);
+            }
+
+            let instruction = vec![opcode, operands[0], operands[1]];
+            program.extend_from_slice(&instruction);
+        }
+    }
+    program
+}
+
+
+fn main() {
+
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("AssA emulator");
+        println!("Usage:\n {} <file_path> [OPTIONS]", args[0]);
+        println!("OPTIONS:\n --print-state - Print CPU state after program execution");
+        return;
+    }
+    // FLAGS
+    let mut print_usage: bool = false;
+
+    if args.len() > 2 {
+        for arg in args.iter().skip(2) {
+            match arg.as_str() {
+                "--print-state" => print_usage = true,
+                _ => {}
+            }
+        }
+    }
+
+    let file_name: Vec<_> = env::args().collect();
+
+    let path = Path::new(file_name[1].as_str());
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Couldn't open {}: {}", display, why),
+        Ok(file) => file
+    };
+
+    let mut source = String::new();
+    match file.read_to_string(&mut source) {
+        Err(why) => panic!("Couldn't read {}: {}", display, why),
+        _ => {}
+    }
+
+    let program = lexer(source);
+    run::run_emulation(program, print_usage);
+}
---
+++ src/run.rs
@@ -0,0 +1,94 @@
+const MEMORY_SIZE: usize = 256;
+
+#[derive(Debug)]
+struct CPU {
+    registers: [u8; 4],  // 4 general-purpose registers
+    memory: [u8; MEMORY_SIZE],
+    program_counter: u8,
+}
+
+#[derive(Debug)]
+pub enum Instructions {
+    MovRegReg,
+    AddRegReg,
+    SubRegReg,
+    IncReg,
+    DecReg,
+    JmpAddr,
+    HLT,
+}
+
+fn execute_instruction(cpu: &mut CPU, opcode: Instructions, operand1: u8, operand2: u8) {
+    match opcode {
+        Instructions::MovRegReg => cpu.registers[operand1 as usize] = cpu.registers[operand2 as usize],
+        Instructions::AddRegReg => cpu.registers[operand1 as usize] += cpu.registers[operand2 as usize],
+        Instructions::SubRegReg => cpu.registers[operand1 as usize] -= cpu.registers[operand2 as usize],
+        Instructions::IncReg => cpu.registers[operand1 as usize] += 1,
+        Instructions::DecReg => cpu.registers[operand1 as usize] -= 1,
+        Instructions::JmpAddr => cpu.program_counter = operand1,
+        Instructions::HLT => {
+            println!("Halted.");
+        }
+    }
+}
+
+fn load_program(cpu: &mut CPU, program: &[u8]) {
+    for (i, &instruction) in program.iter().enumerate() {
+        cpu.memory[i] = instruction;
+    }
+}
+
+fn run_program(cpu: &mut CPU, program_size: usize) {
+    while cpu.program_counter < program_size as u8 {
+        let opcode = cpu.memory[cpu.program_counter as usize];
+        let operand1 = cpu.memory[(cpu.program_counter + 1) as usize];
+        let operand2 = cpu.memory[(cpu.program_counter + 2) as usize];
+
+
+        if opcode as u8 == Instructions::HLT as u8 {
+            println!("Halted.");
+            return;
+        }
+        execute_instruction(cpu, Instructions::from(opcode), operand1, operand2);
+
+        // Move to the next instruction
+        cpu.program_counter += 3;
+
+    }
+}
+
+impl From<u8> for Instructions {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => Instructions::MovRegReg,
+            1 => Instructions::AddRegReg,
+            2 => Instructions::SubRegReg,
+            3 => Instructions::IncReg,
+            4 => Instructions::DecReg,
+            5 => Instructions::JmpAddr,
+            6 => Instructions::HLT,
+            _ => panic!("Unknown instruction: {}", value),
+        }
+    }
+}
+
+pub fn run_emulation(program_vector: Vec<u8>, print_usage: bool) {
+    let mut cpu = CPU {
+        registers: [0, 0, 0, 0],
+        memory: [0; MEMORY_SIZE],
+        program_counter: 0,
+    };
+
+    let program = &program_vector[..];
+    dbg!(program);
+    load_program(&mut cpu, &program);
+    run_program(&mut cpu, program.len());
+    if print_usage {
+        println!("################### CPU STATE AFTER PROGRAM ###################");
+        println!("PC = {}", cpu.program_counter);
+        println!(
+            "reg1 = {}, reg2 = {}, reg3 = {}, reg4 = {}",
+            cpu.registers[0], cpu.registers[1], cpu.registers[2], cpu.registers[3]
+        );
+    }
+}
